2dd86760d027014ab8ad409ad4c0fb7c574031bc
Chanjin Park <parkchamchi@gmail.com>
2023-06-07 15:02:03 +0900

    `end_date` now renders a date input form

--- src/lists/forms.py
+++ src/lists/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import DateInput
 
 from .models import Task
 
@@ -6,3 +7,6 @@ class TaskForm(forms.ModelForm):
 	class Meta:
 		model = Task
 		fields = ("title", "end_date", "priority")
+		widgets = {
+			"end_date": DateInput(attrs={"type": "date"}), #Renders a date input
+		}
