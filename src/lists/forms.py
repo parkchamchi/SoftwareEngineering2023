12728fa576ce48ffe06b94da0dda891ff25c10eb
parkchamchi <parkchamchi@gmail.com>
2023-06-12 16:07:07 +0900

    implemented task edit & delete

--- src/lists/forms.py
+++ src/lists/forms.py
@@ -3,7 +3,15 @@ from django.forms import DateInput
 
 from .models import Task
 
+from datetime import datetime, timedelta
+
 class TaskForm(forms.ModelForm):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields["title"].initial = "New task"
+		self.fields["end_date"].initial = datetime.now().date() + timedelta(days=1) #tomorrow
+		self.fields['priority'].initial = 10
+
 	class Meta:
 		model = Task
 		fields = ("title", "end_date", "priority")
